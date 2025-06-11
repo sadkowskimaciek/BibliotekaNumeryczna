43a1b3288e15208e29b28fe7f2154be33ccbedc4
Kamil <kamil.siarkowicz@op.pl>
2025-06-11 17:34:08 +0200

    Add files via upload

---
+++ tests/InterpolacjaTest.cpp
@@ -0,0 +1,78 @@
+#include "../include/numlib/Interpolacja.h"
+#include <string>
+
+class TestFailedException : public std::runtime_error {
+public:
+    TestFailedException(const string& message) : std::runtime_error(message) {}
+};
+
+void assertTest(bool condition, const string& message) {
+    if (!condition) {
+        throw TestFailedException(message);
+    }
+}
+
+bool runTest(const string& testName, void (*testFunction)()) {
+    cout << "\nUruchamiam " << testName << "..." << endl;
+    try {
+        testFunction();
+        cout << testName << " PASSED" << endl;
+        return true;
+    }
+    catch (const TestFailedException& e) {
+        cout << testName << " FAILED: " << e.what() << endl;
+        return false;
+    }
+    catch (const std::exception& e) {
+        cout << testName << " FAILED z nieoczekiwanym wyj¹tkiem: " << e.what() << endl;
+        return false;
+    }
+}
+
+void InterpolacjaTest1() {
+    vector<double> x = { -1, 0, 1 };
+    vector<double> y = { 1, 0, 1 }; 
+
+    auto coeffs = numlib::DividedDiff(x, y);
+
+    for (int i = 0; i < x.size(); i++) {
+        double result = numlib::Newton(x, coeffs, x[i]);
+        assertTest(abs(result - y[i]) < 1e-10,
+            "B³¹d interpolacji w punkcie x = " + to_string(x[i]));
+    }
+
+    double result = numlib::Newton(x, coeffs, 0.5);
+    assertTest(abs(result - 0.25) < 1e-10,
+        "B³¹d interpolacji w punkcie x = 0.5");
+}
+
+void InterpolacjaTest2() {
+    vector<double> x;
+    vector<double> y;
+    int n = 15; 
+
+    for (int i = 0; i < n; i++) {
+        double xi = -1.0 + 2.0 * i / (n - 1);
+        x.push_back(xi);
+        y.push_back(1.0 / (1.0 + 25.0 * xi * xi));
+    }
+
+    auto coeffs = numlib::DividedDiff(x, y);
+
+    double x_test = 0.75;
+    double y_exact = 1.0 / (1.0 + 25.0 * x_test * x_test); 
+    double y_interp = numlib::Newton(x, coeffs, x_test);   
+
+    cout << "Wartoœæ dok³adna w x = " << x_test << ": " << y_exact << endl;
+    cout << "Wartoœæ interpolowana: " << y_interp << endl;
+    cout << "B³¹d bezwzglêdny: " << abs(y_exact - y_interp) << endl;
+
+    assertTest(abs(y_exact - y_interp) > 0.1,
+        "Interpolacja funkcji Rungego powinna dawaæ znacz¹cy b³¹d");
+}
+
+int main() {
+    runTest("Test interpolacji wielomianem kwadratowym", InterpolacjaTest1);
+	runTest("Test interpolacji funkcji Rungego", InterpolacjaTest2);    
+    return 0;
+}
--- tests/RownaniaLinioweTest.cpp
+++ tests/RownaniaLinioweTest.cpp
@@ -1,5 +1,34 @@
 #include "../include/numlib/RownaniaLiniowe.h"
 #include <cassert>
+#include <stdexcept>
+
+class TestFailedException : public std::runtime_error {
+public:
+    TestFailedException(const string& message) : std::runtime_error(message) {}
+};
+
+void assertTest(bool condition, const string& message) {
+    if (!condition) {
+        throw TestFailedException(message);
+    }
+}
+
+bool runTest(const string& testName, void (*testFunction)()) {
+    cout << "\nUruchamiam " << testName << "..." << endl;
+    try {
+        testFunction();
+        cout << testName << " PASSED" << endl;
+        return true;
+    }
+    catch (const TestFailedException& e) {
+        cout << testName << " FAILED: " << e.what() << endl;
+        return false;
+    }
+    catch (const std::exception& e) {
+        cout << testName << " FAILED z nieoczekiwanym wyj¹tkiem: " << e.what() << endl;
+        return false;
+    }
+}
 
 void LinearEquationTest1() {
     vector<vector<double>> A = {
@@ -10,10 +39,10 @@ void LinearEquationTest1() {
 
     auto x = numlib::Gauss(A, b);
 
-    assert(abs(x[0] - 1.8) < 1e-6);
-    assert(abs(x[1] - 1.4) < 1e-6);
+    assertTest(abs(x[0] - 1.8) < 1e-6, "x[0] powinno byæ 1.8");
+    assertTest(abs(x[1] - 1.4) < 1e-6, "x[1] powinno byæ 1.4");
 
-    cout << "Test1 Passed\n";
+    cout << "x[0] = " << x[0] << ", x[1] = " << x[1] << endl;
 }
 
 void LinearEquationTest2() {
@@ -30,17 +59,40 @@ void LinearEquationTest2() {
     double check2 = -x[0] + 2 * x[1] - x[2];
     double check3 = -x[1] + 2 * x[2];
 
-    assert(abs(check1 - 1) < 1e-6);
-    assert(abs(check2 - 0) < 1e-6);
-    assert(abs(check3 - 1) < 1e-6);
+    assertTest(abs(check1 - 1) < 1e-6, "Równanie 1 nie jest spe³nione");
+    assertTest(abs(check2 - 0) < 1e-6, "Równanie 2 nie jest spe³nione");
+    assertTest(abs(check3 - 1) < 1e-6, "Równanie 3 nie jest spe³nione");
 
-    cout << "Test2 PASSED\n";
+    cout << "check1 = " << check1 << ", check2 = " << check2 << ", check3 = " << check3 << endl;
+}
+
+void LinearEquationTest3() {
+    vector<vector<double>> A = {
+        {2, 1, -1},
+        {-3, -1, 2},
+        {-2, 1, 2}
+    };
+    vector<double> b = { 8, -11, -3 };
+
+    auto x = numlib::Gauss(A, b);
+
+    assertTest(abs(x[0] - 2) < 1e-6, "x[0] powinno byæ 2");
+    assertTest(abs(x[1] - 3) < 1e-6, "x[1] powinno byæ 3");
+    assertTest(abs(x[2] - (-1)) < 1e-6, "x[2] powinno byæ -1");
+
+    cout << "x[0] = " << x[0] << ", x[1] = " << x[1] << ", x[2] = " << x[2] << endl;
 }
 
 int main() {
+    int passedTests = 0;
+    int totalTests = 3;
 
-    LinearEquationTest1();
-    LinearEquationTest2();
+    if (runTest("Test1", LinearEquationTest1)) passedTests++;
+    if (runTest("Test2", LinearEquationTest2)) passedTests++;
+    if (runTest("Test3", LinearEquationTest3)) passedTests++;
 
-    return 0;
+    cout << "\n=== Podsumowanie testów ===" << endl;
+    cout << "Przesz³o: " << passedTests << "/" << totalTests << endl;
+    
+    return passedTests != totalTests;
 }
